02acfd7e104655563f6286133d02497e528abc1c
Aadil Kakkidi <ajk85029@hawkmail.hacc.edu>
2025-10-20 15:55:42 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "https://e89006ca-f8ef-4a3f-8d90-f6a7f1445475-dev.e1-us-east-azure.choreoapis.dev/django-react-full-stack-a/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
